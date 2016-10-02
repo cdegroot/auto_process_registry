fcfd5a2d31a2b2f3ef8c012c8af8b9964b11ccbc
Cees de Groot <cg@cdegroot.com>
2016-10-01 22:50:51 -0400

    Move to macros

--- lib/auto_process_registry.ex
+++ lib/auto_process_registry.ex
@@ -7,80 +7,99 @@ defmodule AutoProcessRegistry do
 	whichever reason, they can be restarted on demand when necessary. It
   is not hard, though, to make the starter function register a process
   under a supervisor.
-	"""
-  use GenServer
 
-  # Client API
+	The registry complies with 'via' naming.
+
+  Moar docs needed.
+	"""
 
 	@doc """
 	Start the GenServer process implementing the registry. The `starter_fun`
-  argument is a function that takes a key and returns a tuple with
-  status and the new process id. It is called whenever `send/3` is called
-  for a key that doesn't have a corresponding process.
-  """
-	@spec start_link((any -> GenServer.on_start)) :: GenServer.on_start
-  def start_link(starter_fun) do
-    GenServer.start_link(__MODULE__, starter_fun)
-  end
+	argument is a function that takes a key and returns a tuple with
+	status and the new process id. It is called whenever `send/3` is called
+	for a key that doesn't have a corresponding process.
+	"""
+	@callback start_link :: GenServer.on_start
 
-  @doc """
-  Send a message to the process that is associated with the given key.
-  """
-  @spec send(pid, any, any) :: any
-  def send(registry_pid, key, msg) do
-    target_pid = case whereis_name(registry_pid, key) do
-      pid when is_pid(pid) ->
-        pid
+  @callback start_new(any) :: GenServer.on_start
 
-      :undefined ->
-				{:ok, pid} = GenServer.call(registry_pid, {:create_new, key})
-				pid
-    end
-		Kernel.send(target_pid, msg)
-  end
+  @doc false
+  defmacro __using__(_) do
+    quote do
+      @behaviour AutoProcessRegistry
 
-  defp whereis_name(registry_pid, key) do
-    GenServer.call(registry_pid, {:whereis_name, key})
-  end
+			use GenServer
 
-  # Server API #
+			# Client API
 
-  def init(starter_fun) do
-    {:ok, {starter_fun, %{}}}
-  end
+			@doc false
+			def start_link do
+				GenServer.start_link(__MODULE__, [], name: __MODULE__)
+			end
 
-  def handle_call({:create_new, key}, _from, {starter_fun, registry}) do
-    case Map.get(registry, key, nil) do
-      nil ->
-				{:ok, pid} = starter_fun.(key)
-        Process.monitor(pid)
-        registry = Map.put(registry, key, pid)
-				{:reply, {:ok, pid}, {starter_fun, registry}}
+			def send(key, msg) do
+				case whereis_name(key) do
+					pid when is_pid(pid) ->
+						Kernel.send(pid, msg)
 
-      pid ->
-				{:reply, {:ok, pid}, {starter_fun, registry}}
-    end
-  end
+					:undefined ->
+						{:badarg, {key, msg}}
+				end
+			end
 
-  def handle_call({:whereis_name, key}, _from, registry) do
-    {:reply, Map.get(registry, key, :undefined), registry}
-  end
+			def whereis_name(key) do
+				GenServer.call(__MODULE__, {:whereis_name, key})
+			end
 
-  def handle_info({:DOWN, _ref, :process, pid, _reason}, {starter_fun, registry}) do
-    {:noreply, {starter_fun, deregister(registry, pid)}}
-  end
+			# Server API #
 
-  def handle_info(_info, {starter_fun, registry}), do: {:noreply, {starter_fun, registry}}
+			def init([]) do
+				{:ok, %{}}
+			end
 
-  # Helper Functions #
-  defp deregister(registry, pid) when is_pid(pid) do
-    case Enum.find(registry, nil, fn({_key, cur_pid}) -> cur_pid == pid end) do
-      nil -> registry
-      {key, _pid} -> deregister(registry, key)
-    end
-  end
+			def handle_call({:create_new, key}, _from, registry) do
+				case Map.get(registry, key, nil) do
+					nil ->
+						{:ok, pid} = start_new(key)
+						Process.monitor(pid)
+						registry = Map.put(registry, key, pid)
+						{:reply, {:ok, pid}, registry}
 
-  defp deregister(registry, key) do
-    Map.delete(registry, key)
-  end
+					pid ->
+						{:reply, {:ok, pid}, registry}
+				end
+			end
+
+			def handle_call({:whereis_name, key}, _from, registry) do
+				case Map.get(registry, key, nil) do
+					nil ->
+						{:ok, pid} = start_new(key)
+						Process.monitor(pid)
+						registry = Map.put(registry, key, pid)
+						{:reply, pid, registry}
+
+					pid ->
+						{:reply, pid, registry}
+				end
+			end
+
+			def handle_info({:DOWN, _ref, :process, pid, _reason}, registry) do
+				{:noreply, deregister(registry, pid)}
+			end
+
+			def handle_info(_info, registry), do: {:noreply, registry}
+
+			# Helper Functions #
+			defp deregister(registry, pid) when is_pid(pid) do
+				case Enum.find(registry, nil, fn({_key, cur_pid}) -> cur_pid == pid end) do
+					nil -> registry
+					{key, _pid} -> deregister(registry, key)
+				end
+			end
+
+			defp deregister(registry, key) do
+				Map.delete(registry, key)
+			end
+	  end
+	end
 end
--- test/auto_process_registry_test.exs
+++ test/auto_process_registry_test.exs
@@ -2,7 +2,17 @@ defmodule AutoProcessRegistryTest do
   use ExUnit.Case
   doctest AutoProcessRegistry
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmodule SampleModule do
+    use AutoProcessRegistry
+
+    def start_new(key) do
+      Agent.start_link(fn -> key end)
+    end
+  end
+
+  test "basic usage" do
+    SampleModule.start_link
+    assert 42 == Agent.get({:via, SampleModule, 42}, fn(key) -> key end)
+    assert 666 == Agent.get({:via, SampleModule, 666}, fn(key) -> key end)
   end
 end
